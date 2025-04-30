18645cc352d4385d87c93289a57345835297ee1d
ThallesRodri <thalles07rodri@gmail.com>
2025-04-29 21:32:14 -0300

    Alteracoes Jogo Cobrinha

--- main.py
+++ main.py
@@ -16,26 +16,38 @@ som_colisao = pygame.mixer.Sound("sons/smw_coin.wav") # todos os sons sem ser o 
 
 largura = 640
 altura = 480
-x = int(largura/2)
-y = int(altura/2)
+
+x_cobra = int(largura/2)
+y_cobra = int(altura/2)
+
+velocidade = 5
+x_controle = velocidade
+y_controle = 0
 
 fonte = pygame.font.SysFont('arial', 40, True, True) # fonte, tamanho, negrito, italico
 pontos = 0
 
 # Gerar numeros aleatorios para a posição do retangulo azul
-x_azul = randint(40,600) # parametros: numeros aleatorios entre 40 e 600
-y_azul = randint(50,430)
+x_maca = randint(40,600) # parametros: numeros aleatorios entre 40 e 600
+y_maca = randint(50,430)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Recicla Mack')
 relogio = pygame.time.Clock()
 
+lista_cobra = []
+comprimento_inicial = 5
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        pygame.draw.rect(tela, (0,255,0), (XeY[0], XeY[1], 20, 20))
+
 # Condição para o jogo não ficar aberto infinitamente
 while True:
     relogio.tick(60) # controlador de frames
-    tela.fill((0,0,0)) # a tela sempre se manter preta
+    tela.fill((255,255,255)) # a tela sempre se manter preta
     mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem, True, (255,255,255)) # texto, tirar serrilhado, cor do texto
+    texto_formatado = fonte.render(mensagem, True, (0,0,0)) # texto, tirar serrilhado, cor do texto
 
     # condição para o jogo ficar rodando até a gente apertar em sair
     for event in pygame.event.get():
@@ -44,47 +56,74 @@ while True:
             exit()
 
     # Fazer o objeto ser controlado pelo jogador
-        '''
         if event.type == KEYDOWN: # se alguma tecla for apertada
             if event.key == K_a:
-                x -= 20
-
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
             if event.key == K_d:
-                x += 20
-
-            if event.key == K_s:
-                y += 20
-
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
             if event.key == K_w:
-                y -= 20
-        '''
+                if y_controle == velocidade:
+                    pass
+                else:
+                    x_controle = 0
+                    y_controle = -velocidade
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = 0
+                    y_controle = velocidade
 
-    if pygame.key.get_pressed()[K_a]: # quando pressionado a tecla, o objeto não para de se mexer
-        x -= 20
+    x_cobra += x_controle
+    y_cobra += y_controle
+       
+    # if pygame.key.get_pressed()[K_a]: # quando pressionado a tecla, o objeto não para de se mexer
+    #     x_cobra -= 10
 
-    if pygame.key.get_pressed()[K_d]: 
-        x += 20
+    # if pygame.key.get_pressed()[K_d]: 
+    #     x_cobra += 10
 
-    if pygame.key.get_pressed()[K_s]: 
-        y += 20
+    # if pygame.key.get_pressed()[K_s]: 
+    #     y_cobra += 10
 
-    if pygame.key.get_pressed()[K_w]: 
-        y -= 20
+    # if pygame.key.get_pressed()[K_w]: 
+    #     y_cobra -= 10
 
     #pygame.draw.circle(tela, (0,255,0), (300, 260), 40) # Circulo, onde o terceiro parametro é a posição xy e o quarto é o raio do circulo
     #pygame.draw.line(tela, (255, 255, 0), (390,0), (390,600),5) # Linha trabalha com ponto, 3º param ponto um, 4º ponto dois, 5º a expessura da linha 
 
     # Desenhando um retangulo vermelho
     # o sistema de cores é RGB onde 255 é o valor máximo. Nesse caso estou desenhando um retangulo vermelho
-    ret_verm = pygame.draw.rect(tela, (255,0,0), (x,y,40,50)) # o segundo parametro são as cores do retangulo e o terceiro o seu tamanho: x, y, largura, altura
-    ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul,y_azul,40,50))
+    cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20)) # o segundo parametro são as cores do retangulo e o terceiro o seu tamanho: x, y, largura, altura
+    maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
 
     # Criando colisões entre objetos
-    if ret_verm.colliderect(ret_azul):
-        x_azul = randint(40,600)
-        y_azul = randint(50,430)
+    if cobra.colliderect(maca):
+        x_maca = randint(40,600)
+        y_maca = randint(50,430)
         pontos += 1
         som_colisao.play()
+
+        comprimento_inicial += 1
+
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+
+    lista_cobra.append(lista_cabeca)
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
+
+    aumenta_cobra(lista_cobra)
 
     # # fazer o objeto se mover sozinho
     # if y >= altura:
